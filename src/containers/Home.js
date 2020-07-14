ef9437f8a70dacc7f2a478ac2991c1c930d664ad
Liam <liamcavens@googlemail.com>
2020-07-14 22:21:20 +0100

    retrieved data

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,18 +1,32 @@
 import React, { Component } from "react";
 
+const top40url = "https://product-fetch-toolstop.herokuapp.com/top40";
 class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {};
     }
 
-    componentDidMount() {}
+    componentDidMount() {
+        this.getTop40();
+    }
+
+    getTop40 = () => {
+        fetch(top40url)
+            .then((res) => res.json())
+            .then((data) => {
+                this.setState({
+                    top40: data.data,
+                });
+            });
+    };
 
     render() {
+        console.log(this.state);
         return (
             <div>
                 <header>Header</header>
-                <nav> Navbar </nav>
+                <nav>Navbar</nav>
             </div>
         );
     }
