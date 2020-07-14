c00cc87186f903572db89fa29b1220c81d9c8906
Liam <liamcavens@googlemail.com>
2020-07-14 22:10:15 +0100

    Initial Commit/Setup

---
+++ src/containers/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {}
+
+    render() {
+        return (
+            <div>
+                <header>Header</header>
+                <nav> Navbar </nav>
+            </div>
+        );
+    }
+}
+
+export default Home;
