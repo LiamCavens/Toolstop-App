4e80ce7220deb49527ce40031a6358449c437cdd
Liam <liamcavens@googlemail.com>
2020-07-16 15:01:53 +0100

    Added top tools header

--- src/components/TopForty/TopForty.js
+++ src/components/TopForty/TopForty.js
@@ -5,6 +5,7 @@ export default class TopForty extends Component {
     render() {
         return (
             <>
+                <h2>Top 40 Tools</h2>
                 <ToolList tools={this.props.allTools} />
             </>
         );
